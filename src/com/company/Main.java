75402d776e3c84c50c18fddec0f8ada5872c2d1f
salymbaeva17 <salymbaeva17@gmail.com>
2022-02-25 10:21:23 +0600

    numbers from 100 to 1000 that divisible without remainder to 5&&6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -112,5 +112,23 @@ public class Main {
 
         int cube = (int) Math.pow(n, 3);
         System.out.println(cube);
+
+        int c = 0;
+        for (int i = 100; i <= 1000; i++) {
+            if (i % 5 == 0 && i % 6 == 0) {
+                System.out.print(i + " ");
+                c++;
+            }
+            if (c == 10) {
+                c = 0;
+                System.out.println();
+            }
+        }
+
+        // factorial решить
+//        for (n; n>=1; n--){
+//
+//        }
+
     }
 }
